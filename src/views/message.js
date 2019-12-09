babb9c16e6f20305e08fa344ec8151fcdbd5d670
kosamir <amir.kos@gmail.com>
2019-12-09 19:47:29 +0100

    dodani letter spacing, line height, lagani refactor,, dodana stranica otp banke za test, malo uredjeni stilovi

--- src/views/message.js
+++ src/views/message.js
@@ -1,26 +1,50 @@
 import html from './message.html';
 import './message.css';
 
-const OMOLAB_BODY_CLASS = `omolab-w-body-${Date.now()}-${Math.ceil(Math.random()*1000)}`;
-const headerStyles =['h1','h2','h3','h4','h5','h6']
+const OMOLAB_BODY_CLASS = `omolab-w-body-${Date.now()}-${Math.ceil(Math.random() * 1000)}`;
+
+/**HEADER STYLES */
+const HEADER_STYLE_ELEMENTS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+
+const transformHeaderStyles = () => HEADER_STYLE_ELEMENTS.map(element => `body.${OMOLAB_BODY_CLASS} ${element}, body.${OMOLAB_BODY_CLASS} ${element} *`)
+const setHeaderStyle = (style, headerFontFamily, headerFontSize, headerFontSpacing, headerLineHeight) => style + `{ font-family:${headerFontFamily} !important ; font-size:${headerFontSize ? headerFontSize : 10}px !important; letter-spacing:${headerFontSpacing ? headerFontSpacing: 'normal'} !important; line-height:${headerLineHeight ? headerLineHeight : '1.6'} !important }\n`
+
+/** WIDGET STYLE */
+const OMO_WIDGET_ELEMENTS = [
+    `body.${OMOLAB_BODY_CLASS} div.omo-widget-container *`
+    // ,
+    // `body.${OMOLAB_BODY_CLASS} div.omoContainer *`
+    // ,
+    // `body.${OMOLAB_BODY_CLASS} div.omoBox *`,
+    // `body.${OMOLAB_BODY_CLASS} div.omoClose *`,
+    // `body.${OMOLAB_BODY_CLASS} div.omoControl *`
+]
+const omoWidgetStyle = '{ font-family: Arial !important; font-size:16px !important;  letter-spacing:normal !important; line-height: 1.6 !important; background-color: #7abf43;}\n'
+/** SET WIDGET STYLE */
+const setOmoWidgetStyle = (omoWidgetElements, omoWidgetStyle) => { return omoWidgetElements.join(',') + ' ' + omoWidgetStyle }
+
+/** SET BODY STYLE */
+const BODY_STYLE = [
+    `body.${OMOLAB_BODY_CLASS}`,
+    `body.${OMOLAB_BODY_CLASS} div > *`
+]
+const setBodyTextStyle = (_Style, bodyFontFamily, bodyFontSize, bodyFontSpacing, bodyLineHeight) => bodyFontFamily ? _Style.join(',') + `{ font-family:${bodyFontFamily} !important; font-size:${bodyFontSize ? bodyFontSize : 10}px !important; letter-spacing:${bodyFontSpacing ? bodyFontSpacing+'px'  : 'normal'} !important; line-height:${bodyLineHeight ? bodyLineHeight : '1.6'} !important }\n` : '';
+
+/** SET BACGROUND COLOR */
+const BACKGROUND_COLOR_ELEMENTS = [
+    `body.${OMOLAB_BODY_CLASS}`,
+    `body.${OMOLAB_BODY_CLASS} div > *`
+]
+const setBackGroundColor = (applyToElements, bgColor) => bgColor ? applyToElements.join(',') + `{ background-color: ${bgColor} }\n` : ''
 
 let elements = [];
 let body;
-//TODO vidjeti kako ovo rjesiti div .omo-widget-container .omoContainer:not(.omoBox*,.omoClose)
-// const setHeaderStyle = (headerFontFamily, headerFontSize) => headerFontFamily ? `body.${OMOLAB_BODY_CLASS} h1, body.${OMOLAB_BODY_CLASS} h1 *, body h2, body h3, body h4, body h5, body h6 { font-family:${headerFontFamily} !important ; font-size:${headerFontSize ? headerFontSize : 10}px !important}\n` : '';
-const setBodyTextStyle = (bodyFontFamily, bodyFontSize) => bodyFontFamily ? `body.${OMOLAB_BODY_CLASS}, body.${OMOLAB_BODY_CLASS} div > * { font-family:${bodyFontFamily} !important; font-size:${bodyFontSize ? bodyFontSize : 10}px !important}\n` : '';
-const widgetStyle = `body.${OMOLAB_BODY_CLASS} div.omoBox *, body.${OMOLAB_BODY_CLASS} div.omoClose * { font-family: Arial !important ; font-size:17px !important;  }\n`
-const backGroundColor = (bgColor) => bgColor ? `body.${OMOLAB_BODY_CLASS}, body.${OMOLAB_BODY_CLASS} div > * { background-color:${bgColor} }\n` : ''
-
-const setHeaderStyle = () => headerStyles.map(element =>`body.${OMOLAB_BODY_CLASS} ${element}, body.${OMOLAB_BODY_CLASS} ${element} *`) 
-const css_headerStyle = (style,headerFontFamily, headerFontSize) => style + `{ font-family:${headerFontFamily} !important ; font-size:${headerFontSize ? headerFontSize : 10}px !important}\n`   
-
-var show_w;
+var toggler;
 
 
-function addOmolabClassScopeToBody(doc){
+function addOmolabClassScopeToBody(doc) {
     const body = doc.querySelector('body');
-    if (body && !body.classList.contains(OMOLAB_BODY_CLASS)){
+    if (body && !body.classList.contains(OMOLAB_BODY_CLASS)) {
         body.classList.add(OMOLAB_BODY_CLASS);
     }
 }
@@ -32,78 +56,80 @@ export function show(text) {
     console.log(text);
     // temporary.getElementsByClassName('js-widget-dialog')[0].innerHTML=html
 
-     addOmolabClassScopeToBody(document);
+    addOmolabClassScopeToBody(document);
     // append elements to body
     body = document.getElementsByClassName('omo-widget-container')[0];
     var i = 0;
     while (temporary.children.length > 0) {
-        elements.push(temporary.children[0]);
-        body.appendChild(temporary.children[0]);
-        console.log(temporary.children[0])
+        let tmp = temporary.children[0]
+        elements.push(tmp);
+        body.appendChild(tmp);
+        // console.log('HTML-->'+tmp.innerHTML + tmp.childElementCount)
 
     }
-    show_w = showWidget();
-    var closeButton = body.getElementsByClassName('omoClose')[0].firstChild;
-    closeButton.addEventListener("click", show_w.open);
-    
+    toggler = toogleWidget();
+    var closeButton = body.getElementsByClassName('omoClose')[0]
+    closeButton.addEventListener("click", toggler.toogle);
+
     var omoElements = Array.from(body.getElementsByClassName('omoElements')[0].children)
     omoElements.forEach(element => {
         console.log(element.nodeName);
-        if (element.nodeName === 'INPUT') element.addEventListener("change", click_me);
-        if (element.nodeName === 'SELECT') element.addEventListener("change", click_me);
-        
+        if (element.nodeName === 'INPUT') element.addEventListener("change", applyOmoStyles);
+        if (element.nodeName === 'SELECT') element.addEventListener("change", applyOmoStyles);
+
     });
-    // console.log(collorPicker.value);
-
     var check = body.getElementsByClassName('omoControl')[0].childNodes[1]
-    console.log(check);
-    // console.log(collorPicker.innerHTML);
-    check.addEventListener('change', click_me);
+    var c = document.getElementById('applyOverides')
+    console.log(check === c);
+    c.addEventListener('change', applyOmoStyles);
 
 }
 
-
-
-
-function showWidget(){
-   let open = true;
-   var widget = document.getElementsByClassName('omoBox')[0];
-   return {
-       open:function(){
-           if(open){
-            //    alert("closing widget");
-               widget.setAttribute('style','display:none')
-               open=false;
-           }else{
-            //    alert("showing widget")
-               widget.setAttribute('style','display:block')
-               open = true;
-           }
-       }
-   }
+const toogleWidget = () => {
+    let open = true
+    var widget = document.getElementsByClassName('omoBox')[0];
+    var close = document.getElementsByClassName('omoClose')[0].firstChild;
+    return {
+        toogle: function () {
+            if (open) {
+                widget.setAttribute('style', 'display:none')
+                close.textContent = 'open'
+                open = false;
+            } else {
+                widget.setAttribute('style', 'display:block')
+                close.textContent = 'close'
+                open = true;
+            }
+        }
+    }
 }
 
-
-
-function getGenerateStyle() {
+function generateOmoStyle() {
     var bgColor = document.getElementById('bgColor').value
     var headerFontSize = document.getElementById('hsize').value;
     var headerFontFamily = document.getElementById('header_ff').value
+    var headerFontSpacing = document.getElementById('hspacing').value
+    var headerLineHeight = document.getElementById('hheight').value
+
     var bodyFontSize = document.getElementById('bsize').value;
     var bodyFontFamily = document.getElementById('body_ff').value
+    var bodyFontSpacing = document.getElementById('bspacing').value
+    console.log(bodyFontSpacing);
+    var bodyLineHeight = document.getElementById('bheight').value
 
-    var style = backGroundColor(bgColor)
-    var headerStyle = css_headerStyle(setHeaderStyle().join(','),headerFontFamily,headerFontSize);
-    var bodyStyle = setBodyTextStyle(bodyFontFamily, bodyFontSize);
-    console.log(headerStyle);
+    var style = '';//setBackGroundColor(BACKGROUND_COLOR_ELEMENTS, bgColor)
+    var headerStyle = setHeaderStyle(transformHeaderStyles().join(','), headerFontFamily, headerFontSize,headerFontSpacing,headerLineHeight);
     style += headerStyle;
-    
-    style += widgetStyle;
+    var bodyStyle = setBodyTextStyle(BODY_STYLE, bodyFontFamily, bodyFontSize,bodyFontSpacing,bodyLineHeight);
     style += bodyStyle;
+    var widgetStyle = setOmoWidgetStyle(OMO_WIDGET_ELEMENTS, omoWidgetStyle);
+    style += widgetStyle
+
     return style;
 }
 
-var forceRedraw = function(element){
+/** hack TODO!! */
+var forceRedraw = function (element) {
 
     if (!element) { return; }
 
@@ -113,71 +139,59 @@ var forceRedraw = function(element){
     element.appendChild(n);
     element.style.display = 'none';
 
-    setTimeout(function(){
+    setTimeout(function () {
         element.style.display = disp;
         n.parentNode.removeChild(n);
-    },0); // you can play with this timeout to make it as short as possible
+    }, 0); // you can play with this timeout to make it as short as possible
 }
 
-function getAppliedStyleSheet(){
+function getLastAppliedStyleSheet() {
     var children = document.getElementsByTagName("head")[0];
     var children_len = children.getElementsByTagName('style').length
-    var style = children.getElementsByTagName('style')[children_len-1];
+    var style = children.getElementsByTagName('style')[children_len - 1];
     return style;
 }
 
 
-function applyOverides() {
-
+const applyOverides = () => {
     if (document.getElementById('omolab_style_w')) {
-        // var children = document.getElementsByTagName("head")[0];
-        var style = getAppliedStyleSheet();//children.getElementsByTagName('style')[1];
-        style.innerHTML = getGenerateStyle();
+        var style = getLastAppliedStyleSheet();
+        style.innerHTML = generateOmoStyle();
         forceRedraw(style);
-    } else {
-        var css = document.createElement('style');
-        css.type = 'text/css';
-        css.id = 'omolab_style_w'
-
-        var style = getGenerateStyle();
-        // console.log(style);
-
-        if (css.styleSheet)
-            css.styleSheet.cssText = style
-        else
-            css.appendChild(document.createTextNode(style));
-
-        /* Append style to the tag name */
-        document.getElementsByTagName("head")[0].appendChild(css);
-        console.log("apply overides\n" + style);
+        return;
     }
-    
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.id = 'omolab_style_w'
 
+    var style = generateOmoStyle();
+    if (css.styleSheet)
+        css.styleSheet.cssText = style
+    else
+        css.appendChild(document.createTextNode(style));
+
+    /* Append style to the tag name */
+    document.getElementsByTagName("head")[0].appendChild(css);
+    console.log("apply overides\n" + style);
 }
 
-function removeOverides() {
-    var omo_style = document.getElementById('omolab_style');
+
+
+const removeOverides = () => {
     var omo_style_w = document.getElementById('omolab_style_w');
     var children = document.getElementsByTagName("head")[0];
-    // var children_len = children.getElementsByTagName('style').length
-    var style = getAppliedStyleSheet()
-    children.removeChild(style);
-
-
+    var style = getLastAppliedStyleSheet()
+    console.log(omo_style_w === style);
+    // if stylesheet is applied remove it, otherwise ignore
+    if (omo_style_w === style)
+        children.removeChild(style);
 }
 
-export function click_me(event) {
-   
+
+export function applyOmoStyles(event) {
+
     var check = document.getElementById('applyOverides').checked;
     check ? applyOverides() : removeOverides()
     console.log(check + ' ' + event.target);
 
-}
-
-
-export function close() {
-    while (elements.length > 0) {
-        elements.pop().remove();
-    }
-    body.removeEventListener('click', close);
 }
