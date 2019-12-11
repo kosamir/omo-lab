fd98e9237a28dd9cadfbe1d7a19d3ba9c0f319ff
kosamir <amir.kos@gmail.com>
2019-12-11 16:28:43 +0100

    zbundlana slova

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,9 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.html$/i, use: 'html-loader' },
-                { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')] },
+                { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')] }
+                 ,
+                { test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,loader: 'url-loader?limit=100000'},
                 {
                     test: /\.js$/i, exclude: /node_modules/, use: {
                         loader: 'babel-loader',
