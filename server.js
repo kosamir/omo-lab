170f0412b86939b1cf1c112c58487b8b9dda2fec
kosamir <amir.kos@gmail.com>
2020-02-13 09:54:16 +0100

    svg hardkoriani u html, dodan alow access origin u server.js zbog ucitavanje configuracije

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-});
+app.use(
+  express.static(path.join(__dirname, "dist"), {
+    setHeaders: function(res, path) {
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Headers", "Content-Type,X-Requested-With");
+      res.set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+      res.set("X-Powered-By", " 3.2.1");
+      res.type("application/json");
+      res.type("json");
+    }
+  })
+);
+
 app.set("port", process.env.PORT || 8080);
 
 var server = app.listen(app.get("port"), function() {
